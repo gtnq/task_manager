1b29c156545ccd820aba6e764b46a0e8a6b3c3b3
Tony Yu <syu2@uoregon.edu>
2023-04-04 11:05:30 -0700

    adding basics

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+export default function App() {
+    return (
+        <>
+            <h1>Task Manager</h1>
+            <h2>tasks</h2>
+            <div id = 'tasks'></div>
+            <h2>workers</h2>
+            <div id = 'workers'></div>
+            <h2>dues</h2>
+            <div id = 'dues'></div>
+        </>
+    );
+}
