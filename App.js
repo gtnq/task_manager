6cffe9d4c5a71c7066bb8e2a2143fd3b1af1164c
Tony Yu <ttonyyu98@gmail.com>
2023-04-04 18:07:55 -0700

    gitignore added, calendar react and time react component added

--- App.js
+++ App.js
@@ -1,48 +1,66 @@
 import React, { useEffect, useState } from "react";
+import Calendar from 'react-calendar'
+import TimePicker from 'react-time-picker'
+
 
 function Tasks() {
-
-    return
+	return;
 }
 
 function Workers() {
-    return
+	return;
 }
 
 function Dues() {
-    return
+	return;
 }
 
 function Body() {
-    const [tasks, addTasks] = useState([])
-    const [workers, addWorker] = useState([])
-    const [dues, addDues] = useState([])
-    let tasksList = tasks.map((items, ind) => ( 
-    <div key = { `tasks_${ind}` } > { items } </div>
-    ))
-    let workerList = workers.map((items, ind) => ( 
-    <div key = { `worker_${ind}` } > { items } </div>
-    ))
-    let duesList = dues.map((items, ind) => ( 
-    <div key = { `dues_${ind}` } > { items } </div>
-    ))
-    return ( <>
+	const [tasks, addTasks] = useState([]);
+	const [workers, addWorker] = useState([]);
+	const [dues, addDues] = useState([]);
 
-        <h2> tasks </h2> 
-        <input type = 'textarea' > </input> 
-        <Tasks />
-        <h2> workers </h2> 
-        <Workers />
-        <h2> dues </h2> 
-        <Dues />
-        </>)
-    }
+	let tasksList = tasks.map((items, ind) => (
+		<div id={`tasks_${ind}`}> {items} </div>
+	));
+	let workerList = workers.map((items, ind) => (
+		<div id={`worker_${ind}`}> {items} </div>
+	));
+	let duesList = dues.map((items, ind) => (
+		<div id={`dues_${ind}`}> {items} </div>
+	));
+
+	return (
+		<>
+			<h2>tasks</h2>
+			<input type="textarea" id = 'tasks_input'></input>
+            <button id = 'save_tasks'>Save</button>
+			<div id="tasks">{tasksList}</div>
 
 
+			<h2> workers </h2>
+			<input type="textarea" id = 'workers_input'></input>
+            <button id = 'save_tasks'>Save</button>
+			<div id="workers">{workerList}</div>
 
-    export default function App() {
-        return ( <>
-            <h1> Task Manager </h1> <Body />
-            </>
-        );
-    }
+            {/*
+			<h2> dues </h2>
+			<input type="textarea" id = 'dues_input'></input>
+            <button id = 'save_tasks'>Save</button>
+			
+            */}
+            <Calendar />
+            <TimePicker />
+            <div id="dues">{duesList}</div>
+		</>
+	);
+}
+
+export default function App() {
+	return (
+		<>
+			<h1> Task Manager </h1>
+			<Body />
+		</>
+	);
+}
