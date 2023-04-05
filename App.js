aefbf4f06ca7689213694b7c4dcd975cc0a86566
Tony Yu <ttonyyu98@gmail.com>
2023-04-04 18:47:43 -0700

    added tasks , workers, due times, no tests are conducted

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Calendar from 'react-calendar'
-import TimePicker from 'react-time-picker'
-
+import Calendar from "react-calendar";
+import TimePicker from "react-time-picker";
 
 function Tasks() {
 	return;
@@ -16,9 +15,13 @@ function Dues() {
 }
 
 function Body() {
-	const [tasks, addTasks] = useState([]);
-	const [workers, addWorker] = useState([]);
-	const [dues, addDues] = useState([]);
+	const [tasks, addTasks] = useState([]),
+        [workers, addWorker] = useState([]),
+        [dues, addDues] = useState([]),
+        [current_task, setCurrentTask] = useState(""),
+        [current_workers, setCurrentWorker] = useState(''),
+        [dueDate, getDueDate] = useState(new Date()),
+        [dueTime, getDueTime] = useState('10:00');
 
 	let tasksList = tasks.map((items, ind) => (
 		<div id={`tasks_${ind}`}> {items} </div>
@@ -30,28 +33,61 @@ function Body() {
 		<div id={`dues_${ind}`}> {items} </div>
 	));
 
+    let addingworker = (names) => {
+        let arr = names.split(',')
+        for (let i = 0; i < arr.length; i++) {
+            if (!workers.includes(arr[i])) {
+                addWorker(arr[i], ...workers)
+            }
+        }
+
+    }
+
 	return (
 		<>
 			<h2>tasks</h2>
-			<input type="textarea" id = 'tasks_input'></input>
-            <button id = 'save_tasks'>Save</button>
+			<input
+				type="textarea"
+				id="tasks_input"
+				placeholder="One Task at a time please"
+				onChange={(evt) => setCurrentTask(evt.target.value)}></input>
+			<button
+				id="save_tasks"
+				onClick={() => addTasks([current_task, ...tasks])}>
+				Save
+			</button>
 			<div id="tasks">{tasksList}</div>
 
 
 			<h2> workers </h2>
-			<input type="textarea" id = 'workers_input'></input>
-            <button id = 'save_tasks'>Save</button>
+			<input
+				type="textarea"
+				id="workers_input"
+				placeholder="comma after each worker please"
+				onChange={(evt) => setCurrentWorker(evt.target.value)}></input>
+			<button
+				id="save_tasks"
+				onClick={() => addingworker(current_workers)}>
+				Save
+			</button>
 			<div id="workers">{workerList}</div>
 
-            {/*
+
+
+			{/*
 			<h2> dues </h2>
 			<input type="textarea" id = 'dues_input'></input>
             <button id = 'save_tasks'>Save</button>
 			
             */}
-            <Calendar />
-            <TimePicker />
-            <div id="dues">{duesList}</div>
+			<Calendar onChange={getDueDate} value = {dueDate}/>
+			<TimePicker onChange={getDueTime} value = {dueTime}/>
+            <button
+				id="save_due"
+				onClick={() => addDue(...dues, {date:dueDate, time:dueTime})}>
+				Save
+			</button>
+			<div id="dues">{duesList}</div>
 		</>
 	);
 }
