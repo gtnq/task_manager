c865a341dc4619c68a5abf98c6cdd98e8c155a0a
Tony Yu <syu2@uoregon.edu>
2023-04-06 12:09:13 -0700

    added a worker selection, prevent task/due date dup, able to select/deslect multiple worker

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import TimePicker from "react-time-picker";
 
-
 function Tasks() {
 	return;
 }
@@ -17,72 +16,115 @@ function Dues() {
 
 function Body() {
 	const [tasks, addTasks] = useState([]),
-		[workers, addWorker] = useState([]),
-		[dues, addDues] = useState([]),
+		[selectTask, getSelectTask] = useState(),
 		[current_task, setCurrentTask] = useState(""),
+		
+		[workers, addWorker] = useState([]),
 		[current_workers, setCurrentWorker] = useState(""),
-		[dueDate, getDueDate] = useState(new Date()),
-		[finish, getFinish] = useState();
+		[selected_workers, setSelectWorker] = useState([]),
+
+		[dues, addDues] = useState([]),
+		[finish, getFinish] = useState(),
+		[dueDate, getDueDate] = useState(new Date());
 
 	let tasksList = tasks.map((items, ind) => (
-		<option
-			id={`tasks_${ind}`}
-			key={`tasks_${ind}`}>
-			{" "}
-			{items}{" "}
-		</option>
-	));
-
-	let workerList = workers.map((items, ind) => (
 		<>
-		<input
-			type = 'checkbox'
-			id={`workers_${ind}`}
-			key={`workers_${ind}`}
-			value = {items}
-			/>{items}<br/>
-			</>
-			));
+			<input
+				type="checkbox"
+				id={`tasks_${ind}`}
+				key={`tasks_${ind}`}
+				onChange={() => getSelectTask(items)}
+				checked={selectTask == items}
+			/>
+			{items}
+			<br />
+		</>
+	));
+	//console.log(workers, 'workers')
+
 	//console.log(dues)
 	let duesList = dues.map((items, ind) => (
 		<>
-		<input
-			type = 'checkbox'
-			id={`dues_${ind}`}
-			key={`dues_${ind}`}
-			onChange={() => getFinish(items.toString())}
-			checked={finish == items.toString()}/>
-			{items.toString()}<br/>
-			</>
+			<input
+				type="checkbox"
+				id={`dues_${ind}`}
+				key={`dues_${ind}`}
+				onChange={() => getFinish(items.toString())}
+				checked={finish == items.toString()}
+			/>
+			{items.toString()}
+			<br />
+		</>
 	));
 	//console.log(workers, 'worker')
 
-	let addingworker = () => {
-		console.log(workers);
-		let arr = current_workers.split(",");
-		for (let i = 0; i < arr.length; i++) {
-			if (!workers.includes(arr[i])) {
-				addWorker([arr[i], ...workers]);
+	const addingworker = async () => {
+		//console.log(workers);
+		let arr = current_workers.trim().split(",");
+		console.log(arr, "split arr");
+		let worker = workers;
+		//addWorker(arr, ...workers)
+		//for (let i = 0; i < arr.length; i++) {
+		arr.map((item) => {
+			if (!worker.includes(item)) {
+				console.log(item, "previous", worker);
+				worker.push(item);
+				console.log(item, worker, "status check");
+				addWorker(worker);
+				console.log(workers, "test adding result");
 			}
-		}
+		});
 	};
 	//console.log(dueDate, "duedate");
 	//console.log(dueTime, "duetime");
 	//console.log(dues, "dues");
-	let updateHour = (e) => {
-		let dateitem = dueDate
-		dateitem.setHours(e.target.value)
-		console.log(dateitem)
-		getDueDate(new Date(dateitem))
+	const updateHour = (e) => {
+		let dateitem = dueDate;
+		dateitem.setHours(e.target.value);
+		console.log(dateitem);
+		getDueDate(new Date(dateitem));
+	};
+	const updateMinutes = (e) => {
+		let dateitem = dueDate;
+		console.log(dueDate);
+		dateitem.setMinutes(e.target.value);
+		console.log(dateitem);
+		getDueDate(new Date(dateitem));
+	};
 
+	let addSelection =(worker) => {
+		if (!worker.checked) {
+			let selected = selected_workers
+			let loc = selected.indexOf(worker.value)
+			selected.splice(loc,1)
+			setSelectWorker(selected)
+		} else {
+			setSelectWorker([worker.value, ...selected_workers])
+		}
 	}
-	let updateMinutes = (e) => {
-		let dateitem = dueDate
-		console.log(dueDate)
-		dateitem.setMinutes(e.target.value)
-		console.log(dateitem)
-		getDueDate(new Date(dateitem))
-	}
+	console.log(selected_workers)
+	const workerList = workers.map((items, ind) => (
+		<div>
+			{/*console.log(items, 'test worker add div')*/}
+			<input
+				type="checkbox"
+				id={`workers_${ind}`}
+				key={`workers_${ind}`}
+				value={items}
+				onChange={() => addSelection(document.querySelector(`#workers_${ind}`))}
+			/>
+			{items}
+			<br />
+		</div>
+	));
+
+	const taskChecker = () => {
+		if (!tasks.includes(current_task)) addTasks([current_task, ...tasks]);
+	};
+
+	const dueChecker = () => {
+		if (!dues.includes(dueDate)) addDues([...dues, dueDate]);
+	};
 
 	return (
 		<>
@@ -94,12 +136,15 @@ function Body() {
 				onChange={(evt) => setCurrentTask(evt.target.value)}></input>
 			<button
 				id="save_tasks"
-				onClick={() => addTasks([current_task, ...tasks])}>
+				onClick={() => taskChecker()}>
 				Save
 			</button>
-			<br/>
-			<select id="tasks" multiple>{tasksList}</select>
-
+			<br />
+			<div
+				id="tasks"
+				multiple>
+				{tasksList}
+			</div>
 
 			<h2> workers </h2>
 			<input
@@ -112,9 +157,11 @@ function Body() {
 				onClick={() => addingworker()}>
 				Save
 			</button>
-			<div id="workers" multiple>{workerList}</div>
-			
-
+			<div
+				id="workers"
+				multiple>
+				{workerList}
+			</div>
 
 			<h2> dues </h2>
 			<Calendar
@@ -130,8 +177,8 @@ function Body() {
 				name="hour"
 				placeholder="--"
 				type="number"
-				value= {dueDate.getHours()}
-				onChange = {(evt)=> updateHour(evt)}
+				value={dueDate.getHours()}
+				onChange={(evt) => updateHour(evt)}
 			/>
 			{/* from https://projects.wojtekmaj.pl/react-time-picker/ */}
 			<input
@@ -142,18 +189,19 @@ function Body() {
 				name="second"
 				placeholder="--"
 				type="number"
-				value= {dueDate.getMinutes()}
-				onChange = {(evt)=> updateMinutes(evt)}
+				value={dueDate.getMinutes()}
+				onChange={(evt) => updateMinutes(evt)}
 			/>
-			
+
 			<button
 				id="save_due"
-				onClick={() =>
-					addDues([...dues, dueDate])
-				}>
+				onClick={() => dueChecker()}>
 				Save
 			</button>
 			<div id="dues">{duesList}</div>
+			<hr />
+			<div id = 'assigned'>assigned_tasks</div>
+	
 		</>
 	);
 }
