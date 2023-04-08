d2374b80e9c869a3436bc22cd92af946b66dbb38
Tony Yu <ttonyyu98@gmail.com>
2023-04-08 03:32:04 -0700

    something idk added for somereason

--- src/server.js
+++ src/server.js
@@ -1,1 +1,37 @@
-let mysql = require('mysql2')
+let mysql = require('mysql')
+let express = require('express')
+let app = express()
+
+
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "Ysc19981208",
+    database: "alumni_web"
+});
+
+ connection.connect(function(err) {
+    console.log('Connecting to database...\n');
+ 
+    // Handle any errors
+    if (err) {
+       console.log(err);
+       console.log('Exiting application...\n');
+    } else {
+       console.log('Connected to database...\n');
+       // Listen for connections
+       // Note: Will terminate with an error if database connection
+       // is closed
+       const ip = 'localhost';
+       const port = 8080;
+       app.listen(port, ip, function () {
+          try {
+             console.log('Color server app listening on port ' + port);
+          } catch (err) {
+             console.log(err);
+          }
+       });
+    }
+ });
